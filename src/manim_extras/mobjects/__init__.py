ef3e1bcb043bee311fe82ff662b95fdc42026e8a
myabdur2121-cpu <myabdur2121@gmail.com>
2026-07-29 10:51:50 +0600

    Update __init__.py

--- src/manim_extras/mobjects/__init__.py
+++ src/manim_extras/mobjects/__init__.py
@@ -2,6 +2,24 @@
 
 from __future__ import annotations
 
+from .blur import (
+    Blur,
+    BlurCard,
+    CameraBlur,
+    IMGBlur,
+    IMGBlurCard,
+    blur_config,
+    gaussian_blur_rgba,
+)
 from .smooth_polygon import SmoothPolygon
 
-__all__ = ["SmoothPolygon"]
+__all__ = [
+    "Blur",
+    "BlurCard",
+    "CameraBlur",
+    "IMGBlur",
+    "IMGBlurCard",
+    "SmoothPolygon",
+    "blur_config",
+    "gaussian_blur_rgba",
+]
