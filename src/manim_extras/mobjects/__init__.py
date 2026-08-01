3bb87030624650ce5285fd9ab7aaf30a75035eff
myabdur2121-cpu <myabdur2121@gmail.com>
2026-08-01 06:45:45 +0600

    Add GlowDot and DotCloud to mobjects init

--- src/manim_extras/mobjects/__init__.py
+++ src/manim_extras/mobjects/__init__.py
@@ -11,15 +11,20 @@ from .blur import (
     blur_config,
     gaussian_blur_rgba,
 )
+from .glow_dot import DotCloud, GlowDot, GlowDots, TrueDot
 from .smooth_polygon import SmoothPolygon
 
 __all__ = [
     "Blur",
     "BlurCard",
     "CameraBlur",
+    "DotCloud",
+    "GlowDot",
+    "GlowDots",
     "IMGBlur",
     "IMGBlurCard",
     "SmoothPolygon",
+    "TrueDot",
     "blur_config",
     "gaussian_blur_rgba",
 ]
