747862e5ec46f61286a46a55526d21a4cb578e8f
myabdur2121-cpu <myabdur2121@gmail.com>
2026-09-03 23:35:36 +0600

    Update __all__ to include geometry-related classes
    Import GeometryOperations and ManimGeometryAdapter for use in the module.

--- src/manim_extras/utils/__init__.py
+++ src/manim_extras/utils/__init__.py
@@ -2,4 +2,6 @@
 
 from __future__ import annotations
 
-__all__: list[str] = []
+from .line_intersection import GeometryOperations, ManimGeometryAdapter
+
+__all__ = ["GeometryOperations", "ManimGeometryAdapter"]
